bc8d08e5103df2ffe0cf07f9a8bf2956e000c68c
Ayush Raj <ayushsinghju7488@gmail.com>
2021-04-12 15:48:22 +0530

    mongoDB connection setup!

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config()
+
+const {DBconnection} = require('./db/mongoDB')
+DBconnection();
 
 const port = process.env.PORT || 7866;
 
 app.get('/', (req, res) => {
-  res.send("Hii");
+  res.send("Bye");
 })
 
 app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
