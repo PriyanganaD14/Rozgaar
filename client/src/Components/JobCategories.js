a68269e5a8c1aa00b5419914634bf75753c81ecf
Priyangana Das <priyanganadas08@gmail.com>
2021-04-15 06:32:59 +0530

    responsive cards

--- client/src/Components/JobCategories.js
+++ client/src/Components/JobCategories.js
@@ -1,80 +1,99 @@
+
 import React from 'react';
 import {
-    Card,CardImg,CardDeck,CardBody
-  } from 'reactstrap';
-  import './Style/JobCategories.css';
-  import driver from './Assets/taxiDriver.png';
-  import deliveryboy from './Assets/deliveryBoy.png';
-  import fieldsales from './Assets/fieldSales.png';
-  import receptionist from './Assets/receptionist.png';
-  import cook from './Assets/cook.png';
-  import maid from './Assets/maid.png';
-  import mason from './Assets/bricklayer.png';
-  import securityguard from './Assets/securityGuard.png';
-  
+  Card, CardImg, CardBody,
+  Col, Row
+} from 'reactstrap';
+import './Style/JobCategories.css';
+import driver from './Assets/taxiDriver.png';
+import deliveryboy from './Assets/deliveryBoy.png';
+import fieldsales from './Assets/fieldSales.png';
+import receptionist from './Assets/receptionist.png';
+import cook from './Assets/cook.png';
+import maid from './Assets/maid.png';
+import mason from './Assets/bricklayer.png';
+import securityguard from './Assets/securityGuard.png';
 
 const JobCategories = ()=>{
-    return (
-         <div>
-             <div>
-              <CardDeck>
-      <Card className="hvr">
+  return (
+    <div>
+      <Row>
+        <Col className="col-lg-3 col-md-6 col-sm-12 mb">
+        <Card className="hvr mb-md-4">
         <CardImg className="mid" src={driver} style={{width:70,height:80}} alt="Card image cap" />
         <CardBody className="mid">
         <div className="font">DRIVER</div>
         </CardBody>
       </Card>
-      <Card className="hvr">
+        </Col>
+
+        <Col className="col-lg-3 col-md-6 col-sm-12">
+        <Card className="hvr mb-md-4">
         <CardImg className="mid"  src={deliveryboy} style={{width:70,height:80}} alt="Card image cap" />
         <CardBody className="mid">
           <div className="font">DELIVERY BOY</div>
         </CardBody>
       </Card>
-      <Card className="hvr">
+        </Col>
+
+        <Col className="col-lg-3 col-md-6 col-sm-12">
+        <Card className="hvr mb-md-4">
         <CardImg className="mid"  src={fieldsales}style={{width:70,height:80}} alt="Card image cap" />
         <CardBody className="mid">
           <div className="font">FIELD SALES</div>
         </CardBody>
       </Card>
-       <Card className="hvr">
+        </Col>
+
+        <Col className="col-lg-3 col-md-6 col-sm-12">
+        <Card className="hvr mb-md-4">
         <CardImg className="mid"  src={receptionist}style={{width:70,height:80}} alt="Card image cap" />
         <CardBody className="mid">
           <div className="font">RECEPTIONIST</div>
         </CardBody>
       </Card>
-      </CardDeck>
-      </div>
-      <div className="mt-4 mb-4">
-              <CardDeck>
-      <Card className="hvr">
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="col-lg-3 col-md-6 col-sm-12">
+        <Card className="hvr mb-md-4">
         <CardImg className="mid" src={cook}style={{width:70,height:80}} alt="Card image cap" />
         <CardBody className="mid">
           <div className="font">COOK</div>
         </CardBody>
       </Card>
-      <Card className="hvr">
+        </Col>
+
+        <Col className="col-lg-3 col-md-6 col-sm-12">
+        <Card className="hvr mb-md-4">
         <CardImg className="mid" src={maid}style={{width:70,height:80}} alt="Card image cap" />
         <CardBody className="mid">
           <div className="font">MAID/SERVANT</div>
         </CardBody>
       </Card>
-      <Card className="hvr">
+        </Col>
+
+        <Col className="col-lg-3 col-md-6 col-sm-12">
+        <Card className="hvr mb-md-4">
         <CardImg className="mid"  src={mason} style={{width:70,height:80}} alt="Card image cap" />
         <CardBody className="mid">
           <div className="font">MASON</div>
         </CardBody>
       </Card>
-       <Card className="hvr">
+        </Col>
+
+        <Col className="col-lg-3 col-md-6 col-sm-12">
+        <Card className="hvr mb-md-4">
         <CardImg className="mid"  src={securityguard} style={{width:70,height:80}} alt="Card image cap" />
         <CardBody className="mid">
           <div className="font">SECURITY GUARD</div>
         </CardBody>
       </Card>
-      </CardDeck>
-         </div>
-         </div>
-    );
-};
-
+        </Col>
+      </Row>
+    </div>
+  );
+}
 
 export default JobCategories;
