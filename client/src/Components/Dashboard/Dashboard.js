702cfe4f311d87dfe4441a5bced1cef6f3688c6a
saurav <saurav8292@gmail.com>
2021-05-19 09:57:16 +0530

    Dashboard mobile view issue fixed

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,31 +1,27 @@
 import React from 'react'
 import './Dashboard.css'
-import './dash.css'
-import {Link} from "react-router-dom"
+import './Dash.css'
+import Dash from  "./Dash"
+import { useState } from "react"; 
 
- 
 const Dashboard = () =>
 {
+  const [state,setState]=useState(false);
+  const handleClick = () => {
+    setState(!state)
+  }
     return(
-        <div className="row"> 
+        <div className="row" id="whole"> 
         <div className="col-3 side">
-        <div className="col-md-2 col-sm-1 hidden-xs display-table-cell v-align box" id="navigation">
-        <div className="logo">
-        </div>
-        <div className="navi">
-            <ul>
-                <li className="active"><Link to="/Dashboard"><i className="fa fa-home" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Dashboard</span></Link></li>
-                <li><Link to="#"><i className="fa fa-tasks" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Jobs</span></Link></li>
-                <li><Link to="/Application"><i className="far fa-address-card" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Applications</span></Link></li>
-                <li><Link to="#"><i className="fas fa-bookmark" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Bookmarks</span></Link></li>
-                <li><Link to="#"><i className="fa fa-cog" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Setting</span></Link></li>
-                <li><Link to="/Profile"><i className="fa fa-user" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Profile</span></Link></li>
-                <li><Link to="#"><i className="fas fa-sign-out-alt" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Logout</span></Link></li>     
-            </ul>
-        </div>
-    </div>
+     <Dash/>
     </div>
     <div className="col-9 mainss">
+    <div className="arrows" onClick={handleClick}>
+    <i className={state ? 'fas fa-arrow-left' : 'fas fa-arrow-right'}></i>
+    <ul className={state ? 'cd side' : 'ab'}>
+    <Dash/>
+    </ul>
+  </div>
     <div className="container">
     <div className="row align-items-start">
       <div className="col">
@@ -111,11 +107,6 @@ const Dashboard = () =>
     </table>
       </div>
 
-    </div>
-    <div className="row align-items-end">
-      <div className="col">
-        6
-      </div>
     </div>
   </div>
     </div>
