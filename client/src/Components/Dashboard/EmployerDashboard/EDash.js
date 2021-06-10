ae2e4784bd9c90c4fcb71a59cf4c8636fbdfaea0
SUNNY <kumarsunny800001@gmail.com>
2021-06-10 22:55:23 +0530

    Navbar and e dashboard work done

--- client/src/Components/Dashboard/EmployerDashboard/EDash.js
+++ client/src/Components/Dashboard/EmployerDashboard/EDash.js
@@ -28,8 +28,8 @@ const EDash = () =>
                <li><Link to="/JobsFeed"><i className="fa fa-tasks" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Jobs</span></Link></li>
                <li><Link to="/employer/Application"><i className="far fa-address-card" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Applications</span></Link></li>
                <li><Link to="/employer/postJob"><i className="fas fa-bookmark" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Post Job</span></Link></li>
-               <li><Link to="#"><i className="fa fa-cog" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Setting</span></Link></li>
-               <li><Link to="/Profile"><i className="fa fa-user" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Profile</span></Link></li>
+               {/* <li><Link to="#"><i className="fa fa-cog" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Setting</span></Link></li>
+               <li><Link to="/Profile"><i className="fa fa-user" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Profile</span></Link></li> */}
                <li><Link  onClick={logout}><i className="fas fa-sign-out-alt" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Logout</span></Link></li>     
            </ul>
        </div>
